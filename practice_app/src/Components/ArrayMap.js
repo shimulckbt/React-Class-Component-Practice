24c3b2398a1a59d214d59d3ef25c86199f906af7
Shimul Chakraborty <shimul.ckbt@gmail.com>
2021-11-08 04:03:03 +0600

    another approach to use map to get array value and show as dropdown

--- practice_app/src/Components/ArrayMap.js
+++ practice_app/src/Components/ArrayMap.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 
 export default class ArrayMap extends Component {
+
+    arrayMap = (data) => {
+        return <option>{data}</option>
+    }
     render() {
 
         const myArray = ['shimul', 'tutul', 'palash', 'hello'];
-        const dataItems = myArray.map((myData) => {
-            return <option>{myData}</option>
-        })
+        const dataItems = myArray.map(this.arrayMap)
+        // const dataItems = myArray.map((myData) => {
+        //     return <option>{myData}</option>
+        // })
 
         return (
             <div>
