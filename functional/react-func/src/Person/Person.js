ee8f798e1d4dfc8d102eafe75339cb2590694300
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-01 23:00:13 +0600

    props children passinig

--- functional/react-func/src/Person/Person.js
+++ functional/react-func/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const person = (props) => {
-   return <p>i'm {props.name}! and I am {props.age} years old</p>
-}
+   return (
+      <div>
+         <p>i'm {props.name}! and I am {props.age} years old and {props.children}</p>
+
+      </div>
+   );
+};
 
 export default person;
