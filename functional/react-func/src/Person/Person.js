fa65d246c380c6dcd2f8d730d22fc1ee475a6b68
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-07 01:23:41 +0600

    media query in radium package adding StyleRoot tag

--- functional/react-func/src/Person/Person.js
+++ functional/react-func/src/Person/Person.js
@@ -3,8 +3,13 @@ import Radium from 'radium'
 import './Person.css';
 
 const person = (props) => {
+   const style = {
+      '@media (min-width:500px)': {
+         width: '350px'
+      }
+   };
    return (
-      <div className="Person">
+      <div className="Person" style={style}>
          <p onClick={props.click}>i'm {props.name}! and I am {props.age} years old and {props.children}</p>
          <input type="text" onChange={props.changed} value={props.name} />
       </div>
