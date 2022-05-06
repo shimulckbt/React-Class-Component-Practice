fbf90eccbcc580ed39dd0ff67e716b5d2517f96e
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-06 23:47:59 +0600

    two way data binding with a great approach

--- functional/react-func/src/App.js
+++ functional/react-func/src/App.js
@@ -68,9 +68,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Shimul', age: 28 },
-      { name: 'Palash', age: 31 },
-      { name: 'Tutul', age: 22 }
+      { id: 1, name: 'Shimul', age: 28 },
+      { id: 2, name: 'Palash', age: 31 },
+      { id: 3, name: 'Tutul', age: 22 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -88,14 +88,21 @@ class App extends Component {
   //   })
   // }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Shimul', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Tutul', age: 26 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
   }
 
   togglePersonsHandler = () => {
@@ -104,7 +111,10 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    // const persons = this.state.persons;//bad practice
+
+    // const persons = this.state.persons.splice();//good practice
+    const persons = [...this.state.persons]//good practice
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
   }
@@ -124,7 +134,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} />
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
           {/* <Person
             name={this.state.persons[0].name}
