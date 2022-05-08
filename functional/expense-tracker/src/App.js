3241bbcb5fa8938964dd3a2b8212586f66c01f2c
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-08 17:37:31 +0600

    component and composition

--- functional/expense-tracker/src/App.js
+++ functional/expense-tracker/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Expenses from './components/Expenses';
 import './App.css';
 
 function App() {
+  const expenses = [
+    { id: 1, title: 'Car Insurance', amount: 3000, date: new Date(2022, 5, 8) },
+    { id: 2, title: 'Home Insurance', amount: 13000, date: new Date(2022, 3, 22) },
+    { id: 3, title: 'Education Insurance', amount: 4000, date: new Date(2022, 2, 1) },
+    { id: 4, title: 'Health Insurance', amount: 6000, date: new Date(2022, 1, 15) },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Let's get started</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ functional/expense-tracker/src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from './Card';
+
+const ExpenseItem = (props) => {
+   return (
+      <Card className='expense-item'>
+         <ExpenseDate date={props.date} />
+         <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>{props.amount}TK.</div>
+         </div>
+      </Card>
+   )
+}
+
+export default ExpenseItem;
