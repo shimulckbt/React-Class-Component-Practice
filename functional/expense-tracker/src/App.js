64ea283479c4b42dd9ce0cca3e89d49687a8e5ab
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-10 02:09:20 +0600

    minimized code and unique warning solved

--- functional/expense-tracker/src/App.js
+++ functional/expense-tracker/src/App.js
@@ -1,19 +1,24 @@
+import React, { useState } from 'react';
+
 import Expenses from './components/Expenses/Expenses';
 import './App.css';
 import ExpenseForm from './components/NewExpense/ExpenseForm';
 import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
-  const expenses = [
-    { id: 1, title: 'Car Insurance', amount: 3000, date: new Date(2022, 5, 8) },
-    { id: 2, title: 'Home Insurance', amount: 13000, date: new Date(2022, 3, 22) },
-    { id: 3, title: 'Education Insurance', amount: 4000, date: new Date(2022, 2, 1) },
-    { id: 4, title: 'Health Insurance', amount: 6000, date: new Date(2022, 1, 15) },
-  ];
+const DUMMY_EXPENSES = [
+  { id: 1, title: 'Car Insurance', amount: 3000, date: new Date(2022, 5, 8) },
+  { id: 2, title: 'Home Insurance', amount: 13000, date: new Date(2022, 3, 22) },
+  { id: 3, title: 'Education Insurance', amount: 4000, date: new Date(2022, 2, 1) },
+  { id: 4, title: 'Health Insurance', amount: 6000, date: new Date(2022, 1, 15) },
+];
 
+function App() {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
--- functional/expense-tracker/src/components/Expenses/Expenses.js
+++ functional/expense-tracker/src/components/Expenses/Expenses.js
@@ -16,6 +16,7 @@ const Expenses = (props) => {
          <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             {props.items.map(expense => (<ExpenseItem
+               key={expense.id}
                title={expense.title}
                amount={expense.amount}
                date={expense.date}
