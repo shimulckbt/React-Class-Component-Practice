c3b554c00e00d7b01e2a5d2c8e78024e84cc8932
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-10 02:24:16 +0600

    filtered year

--- functional/expense-tracker/src/App.js
+++ functional/expense-tracker/src/App.js
@@ -8,8 +8,8 @@ import NewExpense from './components/NewExpense/NewExpense';
 const DUMMY_EXPENSES = [
   { id: 1, title: 'Car Insurance', amount: 3000, date: new Date(2022, 5, 8) },
   { id: 2, title: 'Home Insurance', amount: 13000, date: new Date(2022, 3, 22) },
-  { id: 3, title: 'Education Insurance', amount: 4000, date: new Date(2022, 2, 1) },
-  { id: 4, title: 'Health Insurance', amount: 6000, date: new Date(2022, 1, 15) },
+  { id: 3, title: 'Education Insurance', amount: 4000, date: new Date(2020, 2, 1) },
+  { id: 4, title: 'Health Insurance', amount: 6000, date: new Date(2021, 1, 15) },
 ];
 
 function App() {
--- functional/expense-tracker/src/components/Expenses/Expenses.js
+++ functional/expense-tracker/src/components/Expenses/Expenses.js
@@ -11,16 +11,21 @@ const Expenses = (props) => {
    const filterChangeHandler = selectedYear => {
       setFilteredYear(selectedYear);
    };
+
+   const filteredExpense = props.items.filter(expense => {
+      return expense.date.getFullYear().toString() === filteredYear;
+   });
    return (
       <div>
          <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {props.items.map(expense => (<ExpenseItem
-               key={expense.id}
-               title={expense.title}
-               amount={expense.amount}
-               date={expense.date}
-            />))}
+            {filteredExpense.map(expense => (
+               <ExpenseItem
+                  key={expense.id}
+                  title={expense.title}
+                  amount={expense.amount}
+                  date={expense.date}
+               />))}
          </Card>
       </div>
    );
