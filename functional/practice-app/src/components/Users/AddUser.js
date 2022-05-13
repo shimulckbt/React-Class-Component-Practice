fa4fd5b790639feaa07b2faa19a185d49d3a7de1
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-13 17:10:47 +0600

    css module added

--- functional/practice-app/src/components/Users/AddUser.js
+++ functional/practice-app/src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
 const AddUser = (props) => {
    const submitHandler = (event) => {
       event.preventDefault();
    };
 
    return (
-      <form onSubmit={submitHandler}>
-         <label htmlFor="username">Username</label>
-         <input id="username" type="text" />
-         <label htmlFor="age">Age (Years)</label>
-         <input id="age" type="number" />
-         <button type="submit">Add User</button>
-      </form>
+      <Card className={classes.input}>
+         <form onSubmit={submitHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" />
+            <label htmlFor="age">Age (Years)</label>
+            <input id="age" type="number" />
+            <button type="submit">Add User</button>
+         </form>
+      </Card>
    );
 };
 
