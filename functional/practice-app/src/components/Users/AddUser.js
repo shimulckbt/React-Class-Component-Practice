5251c1ebb493abe23c6f0f10f9076d61810f0d73
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-13 17:30:50 +0600

    user input handlers

--- functional/practice-app/src/components/Users/AddUser.js
+++ functional/practice-app/src/components/Users/AddUser.js
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+   const [userName, setUserName] = useState('');
+   const [age, setAge] = useState('');
    const submitHandler = (event) => {
       event.preventDefault();
+
+      console.log(userName, age);
+   };
+
+   const userNameChangeHandler = (event) => {
+      setUserName(event.target.value);
+   };
+
+   const ageeChangeHandler = (event) => {
+      setAge(event.target.value);
    };
 
    return (
       <Card className={classes.input}>
          <form onSubmit={submitHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
+            <input id="username" type="text" onChange={userNameChangeHandler} />
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
+            <input id="age" type="number" onChange={ageeChangeHandler} />
             <Button type="submit">Add User</Button>
          </form>
       </Card>
