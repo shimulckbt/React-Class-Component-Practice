449bbc9fced78348cc418b43de9288aa6e644d8a
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-16 02:11:24 +0600

    userslist component and its style

--- functional/practice-app/src/App.js
+++ functional/practice-app/src/App.js
@@ -1,10 +1,13 @@
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <AddUser />
+      <UsersList users={[usersList]} />
     </div>
   );
 }
