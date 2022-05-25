ec282bd9a358255e9fb983b2964d73075e4abc9a
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-25 19:12:17 +0600

    useEffect bounching

--- functional/use-effecr-prac/src/components/Login/Login.js
+++ functional/use-effecr-prac/src/components/Login/Login.js
@@ -12,9 +12,11 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
