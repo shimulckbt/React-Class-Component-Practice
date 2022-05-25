f616108c64c3aef3a1eaea3fd29cd915d24ab26b
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-25 19:23:35 +0600

    useEffect summary

--- functional/use-effecr-prac/src/components/Login/Login.js
+++ functional/use-effecr-prac/src/components/Login/Login.js
@@ -12,6 +12,14 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
+    console.log('effect running');
+
+    return () => {
+      console.log('clean up');
+    };
+  }, []);
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       setFormIsValid(
         enteredEmail.includes('@') && enteredPassword.trim().length > 6
