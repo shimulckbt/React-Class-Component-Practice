c2558e2840db97a7da2203b3b208a7e1129af857
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-30 23:58:44 +0600

    restored

--- functional/use-effecr-prac/src/components/Login/Login.js
+++ functional/use-effecr-prac/src/components/Login/Login.js
@@ -17,7 +17,9 @@ const emailReducer = (state, action) => {
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
 
