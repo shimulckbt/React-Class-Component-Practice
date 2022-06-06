f6a0f2c6c52c471f75618fbe4c1364d085fd1419
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-06-06 21:09:20 +0600

    some changes

--- food-order/src/App.js
+++ food-order/src/App.js
@@ -5,17 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Great interacting with you!
         </a>
       </header>
     </div>
---
+++ food-order/src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = props => {
+   return <React.Fragment>
+      <header>
+         <h1>ReactMeals</h1>
+         <button>Cart</button>
+      </header>
+
+      <div>
+         <img />
+      </div>
+   </React.Fragment>
+}
+
+export default Header;
